12f909d2b9f49c19bfc026c1551a548a2cb58c92
ac9689 <ashwani9689@outlook.com>
2024-08-08 12:56:44 +0530

    new2

--- day1.py
+++ day1.py
@@ -1,7 +1,21 @@
 
 a=10
 b=20
-c=a+b
+'''c=a+b
 d=a-b
 f=a*b
-print(c,d,f)
+print(c,d,f)'''
+
+#addition of two integer number
+c=a+b
+print("addition = ",c)
+d={"ass",45,65}
+print(d)
+f={"mjr",65,87}
+print(f)
+d.update(f)
+print(d)
+thisset={"ban","john"}
+mylist=["org","gavan"]
+thisset.update(mylist)
+print(thisset)
